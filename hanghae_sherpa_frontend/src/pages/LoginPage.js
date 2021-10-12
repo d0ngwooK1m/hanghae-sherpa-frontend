a866effa7a08034f6004033a4efc91f1bd829393
jeoungwoo <ajw2312@gmail.com>
2021-10-12 15:19:31 +0900

    LoginPage 커밋

---
+++ hanghae_sherpa_frontend/src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { LoginForm } from '../components';
+
+const LoginPage = () => {
+  return (
+    <React.Fragment>
+      <LoginForm />
+    </React.Fragment>
+  );
+};
+
+export default LoginPage;
